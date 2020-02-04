771e73cbd5a77656775553bafa75e1bd5f271140
Matthew Toczek <matthew.toczek@Matthews-MacBook-Pro.local>
2020-02-04 09:31:37 +0000

    comets for interfaces

--- src/Interfaces.kt
+++ src/Interfaces.kt
@@ -1,8 +1,12 @@
+// interface declaration
 interface Signatory {
+//  obligatory method with no args or method
     fun sign()
 }
 
+// interfaces implemented by class using ":"
 class Person : Signatory {
+//  no annotation (@) needed in Kotlin
     override fun sign() {
         println("I can sign documents!")
     }
